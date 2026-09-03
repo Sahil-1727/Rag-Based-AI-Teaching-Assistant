01b4625feeb0b4232ef0cfb53b44ed2aec981d05
Sahil <sahilteltumde27@gmail.com>
2026-09-03 19:49:01 +0530

    Add vector search functionality to find relevant chunks

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import whisper
+import requests
 print("installed")
+# def create_embedding():
+#     r = requests.post("http://localhost:11434/api/embed", json={
+#         "model": "bge-m3",
+#         "input": ["Why is the sky blue?", "Why is the grass green?"]
+#     })
+#
+#     response_data = r.json()['embeddings']
+#     print(response_data)
+#
+# create_embedding()
